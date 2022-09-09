d3e42f5a116f881f282a4d0d3ab79f3a8c9465fa
ben_ali_s <ben_ali_s@D28-022024-02>
2022-09-09 16:39:54 +0200

    Ajoutez des fichiers projet.

---
+++ Banque/Compte.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banque
+{
+    class Compte
+    {
+        private int _numero;
+        private string _nom;
+        private float _solde;
+        private float _decouvert;
+
+        public Compte(int numero, string nom, float solde, float decouvert)
+        {
+            this._numero = numero;
+            this._nom = nom;
+            this._solde = solde;
+            this._decouvert = decouvert;
+        }
+        public void Transferer(double montant,Compte c)
+        {
+
+        }
+        public bool Superieur(Compte c)
+        {
+            return false;
+        }
+
+        public void Crediter(double montant)
+        {
+
+        }
+
+        public void Debiter(double montant)
+        {
+
+        }
+    }
+}
